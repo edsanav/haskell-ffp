de4651558449b3100504922258ecf9fd77aa1800
Eduardo Santamaria <edsanav@gmail.com>
2022-10-02 11:06:50 +0200

    Finished ch09 exercises

--- src/chapters/ch09/MyDefinitions.hs
+++ src/chapters/ch09/MyDefinitions.hs
@@ -49,4 +49,37 @@ squishMap2 fn l = squish (ma fn l [])
         ma f (x:xs) a = ma f xs ((f x):a)
         
 squishAgain :: [[a]] -> [a]
-squishAgain = squishMap id 
+squishAgain = squishMap id
+
+myMaximumBy:: Ord a => (a -> a -> Ordering) -> [a] -> a
+myMaximumBy _ [] = error "empty list"
+myMaximumBy _ (x:[]) = x
+myMaximumBy f (x:y:xs) = case (compare x y) of
+  LT -> myMaximumBy f (y:xs)
+  _ -> myMaximumBy f (x:xs)
+
+myMinimumBy:: Ord a => (a -> a -> Ordering) -> [a] -> a
+myMinimumBy _ [] = error "empty list"
+myMinimumBy _ (x:[]) = x
+myMinimumBy f (x:y:xs) = case (compare x y) of
+  GT -> myMinimumBy f (y:xs)
+  _ -> myMinimumBy f (x:xs)
+
+myGenericBy:: Ord a => Ordering -> (a -> a -> Ordering) -> [a] -> a
+myGenericBy _ _ [] = error "empty list"
+myGenericBy _ _ (x:[]) = x
+myGenericBy o f (x:y:xs)
+  | compare x y /= o = myGenericBy o f (y:xs)
+  | otherwise = myGenericBy o f (x:xs)
+
+myMaximumBy':: Ord a => (a -> a -> Ordering) -> [a] -> a
+myMaximumBy' = myGenericBy GT
+
+myMinimumBy':: Ord a => (a -> a -> Ordering) -> [a] -> a
+myMinimumBy' = myGenericBy LT
+
+myMaximum:: Ord a => [a] -> a
+myMaximum = myMaximumBy' compare
+
+myMinimum:: Ord a => [a] -> a
+myMinimum = myMinimumBy' compare
