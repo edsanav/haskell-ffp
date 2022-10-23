1693dc610b6acd9181a6b23c695c1bcc3aa7ca29
Eduardo Santamaria <edsanav@gmail.com>
2022-10-23 12:38:57 +0200

    Chapter 10 exercises

--- src/chapters/ch10/DefinitionsFold.hs
+++ src/chapters/ch10/DefinitionsFold.hs
@@ -17,3 +17,37 @@ myElem' x = myAny (==x)
 
 myReverse :: [a] -> [a]
 myReverse = foldl (flip (:)) [] 
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr ((:) . f) []
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f = foldr (\a b -> if f(a) then a:b else b) []
+
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr ((++). f) []
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+
+decideFn :: (a -> a -> Ordering) -> Ordering -> a -> a -> a
+decideFn f o a b
+    | f a b == o = a
+    | otherwise = b
+
+-- z needs to be last element must be last element in list to compare, otherwise it fails on this case
+--Prelude> myMaximumBy (\_ _ -> GT) [1..10]
+-- 1
+-- Instead it returns 2
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy _ [] = error "empty list"
+myMaximumBy f xs =  foldr (decideFn f GT) (last xs) xs
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy _ [] = error "empty list"
+myMinimumBy f xs =  foldr (decideFn f LT) (last xs) xs
