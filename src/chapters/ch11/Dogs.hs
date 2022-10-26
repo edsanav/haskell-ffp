b629edde0a47d25796ae3a1670ffb3173893a5dd
Eduardo Santamaria <edsanav@gmail.com>
2022-10-26 20:27:11 +0200

    Ex

--- src/chapters/ch11/Dogs.hs
+++ src/chapters/ch11/Dogs.hs
@@ -18,19 +18,25 @@ data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
 
 data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq, Show)
 
-data Vehicle = Car Manufacturer Price | Plane Airline deriving (Eq, Show)
+data Vehicle = Car Manufacturer Price | Plane Manufacturer Airline deriving (Eq, Show)
 
 myCar = Car Mini (Price 14000)
 urCar = Car Mazda (Price 20000)
 clownCar = Car Tata (Price 7000)
-doge = Plane PapuAir
+doge = Plane Mini PapuAir
 
 isCar :: Vehicle -> Bool
 isCar (Car _ _) = True
 isCar _ = False
 
 isPlane :: Vehicle -> Bool
-isPlane = undefined
+isPlane (Plane _ _) = True
+isPlane _ = False
 
 areCars :: [Vehicle] -> [Bool]
-areCars = undefined
+areCars [] = []
+areCars (x:xs) = isCar x:areCars(xs)
+
+getManu :: Vehicle -> Manufacturer
+getManu (Car m _) = m
+getManu (Plane m _) = m
