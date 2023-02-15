2568da230a9ef242dca1a69cd2fd4b90be271a11
Eduardo Santamaria <edsanav@gmail.com>
2023-02-15 20:29:30 +0100

    Ch14 and morse init

---
+++ src/chapters/ch14-morse/src/Morse.hs
@@ -0,0 +1,19 @@
+module Morse
+  ( Morse,
+    charToMorse,
+    morseToChar,
+    stringToMorse,
+    letterToMorse,
+    morseToLetter,
+  )
+where
+
+import qualified Data.Map as M
+
+type Morse = String
+
+charToMorse = undefined
+morseToChar = undefined
+stringToMorse = undefined
+letterToMorse = undefined
+morseToLetter = undefined
--- src/chapters/ch14/src/Addition.hs
+++ src/chapters/ch14/src/Addition.hs
@@ -1,6 +1,7 @@
 module Addition (sayHello, mainS) where
 
 import Test.Hspec
+import Test.QuickCheck
 
 sayHello :: IO ()
 sayHello = putStrLn "hello!"
@@ -12,14 +13,15 @@ dividedBy num denom = go num denom 0
       | n < d = (count, n)
       | otherwise = go (n - d) d (count + 1)
 
-myMult::(Integral a) => a -> a -> a
+myMult :: (Integral a) => a -> a -> a
 myMult x y = case sameSign of
   True -> go x y (abs y)
   False -> negate $ go x y (abs y)
-  where go a b count
-          | count == 0 = 0
-          | otherwise = (abs a) + (go a b (count-1))
-        sameSign = (x >= 1 && y >= 1) || (x <= (-1) && y<=(-1))
+  where
+    go a b count
+      | count == 0 = 0
+      | otherwise = (abs a) + (go a b (count -1))
+    sameSign = (x >= 1 && y >= 1) || (x <= (-1) && y <= (-1))
 
 mainS :: IO ()
 mainS = hspec $ do
@@ -28,15 +30,22 @@ mainS = hspec $ do
       (1 + 1) > 1 `shouldBe` True
     it "2 + 2 is equal to 4" $ do
       2 + 2 `shouldBe` 4
+    it
+      "x + 1 is always\
+      \ greater than x"
+      $ do
+        property $ \x -> x + 1 > (x :: Int)
 
 mainDiv :: IO ()
 mainDiv = hspec $ do
   describe "Addition" $ do
     it "15 divided by 3 is 5" $ do
       dividedBy 15 3 `shouldBe` (5, 0)
-    it "22 divided by 5 is \
-        \4 remainder 2" $ do
-      dividedBy 22 5 `shouldBe` (4, 2)
+    it
+      "22 divided by 5 is \
+      \4 remainder 2"
+      $ do
+        dividedBy 22 5 `shouldBe` (4, 2)
 
 mainMult :: IO ()
 mainMult = hspec $ do
@@ -46,3 +55,62 @@ mainMult = hspec $ do
     it "5 multiplied by -3 is -15" $ do
       myMult 5 (-3) `shouldBe` (-15)
 
+trivialInt :: Gen Int
+trivialInt = return 1
+
+oneThroughThree :: Gen Int
+oneThroughThree = elements [1, 2, 3]
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a' .. 'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
+
+-- i.e sample' (genTuple::Gen (Int, Float)) or...
+--Prelude> type G = Gen (Int, Float)
+--Prelude> sample (genTuple :: G)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency
+    [ (1, return Nothing),
+      (3, return (Just a))
+    ]
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
