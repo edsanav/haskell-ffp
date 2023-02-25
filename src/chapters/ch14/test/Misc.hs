b6be147daa4bc382abc4a058b251fd440f93b18c
Eduardo Santamaria <edsanav@gmail.com>
2023-02-25 20:45:42 +0100

    Chapter 14, some examples

--- src/chapters/ch14/test/Misc.hs
+++ src/chapters/ch14/test/Misc.hs
@@ -35,15 +35,35 @@ prop_prodAssociative x y z = x * (y * z) == (x * y) * z
 prop_prodCommutative::(Num a, Eq a) => a -> a -> Bool
 prop_prodCommutative x y = x * y == y * x
 
--- TODO test with positives?
---prop_thereAndBackAgain :: Property
---prop_thereAndBackAgain = forAll charGen
---  (\c -> (charToMorse c
---    >>= morseToChar) == Just c)
-    
-prop_Quot1::(Integral a) => a -> a -> Bool
-prop_Quot1 x y = quot x y * y + rem x y == x
 
+prop_Quot1::(Integral a) => NonZero a -> NonZero a-> Bool
+prop_Quot1 (NonZero x) (NonZero y) = quot x y * y + rem x y == x
+
+prop_Quot2::(Integral a) => NonZero a -> NonZero a -> Bool
+prop_Quot2 (NonZero x) (NonZero y) = div x y * y + mod x y == x
+
+prop_PotCom::(Integral a) => a -> a -> Bool
+prop_PotCom x y = x ^ y == y ^ x
+
+prop_PotAssoc::(Integral a) => Positive a -> Positive a -> Positive a -> Bool
+prop_PotAssoc (Positive x) (Positive y) (Positive z) = x ^ (y ^ z) == (x ^ y) ^ z
+
+prop_RevRev::(Eq a ) => [a] -> Bool
+prop_RevRev x = (reverse . reverse) x == id x
+
+prop_F::(Eq b) => Fun a b -> a -> Bool
+prop_F f x = applyFun f x == (applyFun f $ x)
+
+prop_FComp::(Eq c) => Fun a b -> Fun b c -> a -> Bool
+prop_FComp g f x = (applyFun f . applyFun g) x == (\y -> applyFun f (applyFun g y)) x
+
+prop_foldr1:: (Eq a) => [a] -> [a] -> Bool
+prop_foldr1 xs ys = ((foldr (:) ys xs)) == ((++) xs ys)
+-- either you pass in different order or it doesn't pass the test
+--prop_foldr1 xs ys = ((foldr (:) xs ys)) == ((++) xs ys)
+
+prop_foldr2:: (Eq a) => [[a]] -> Bool
+prop_foldr2 xs = ((foldr (++) [] xs)) == concat xs
 
 main :: IO ()
 main = do
@@ -56,4 +76,14 @@ main = do
   quickCheck (prop_plusCommutative:: Int -> Int -> Bool)
   quickCheck (prop_prodAssociative:: Int -> Int -> Int -> Bool)
   quickCheck (prop_prodCommutative:: Int -> Int -> Bool)
-  quickCheck (prop_Quot1::Int->Int->Bool)
+  quickCheck (prop_Quot1::NonZero Int -> NonZero Int -> Bool)
+  quickCheck (prop_Quot2::NonZero Int -> NonZero Int -> Bool)
+  -- these does not hold even if only checking Nonzero or positive
+--  quickCheck (prop_PotCom:: Int ->  Int ->  Bool)
+--  quickCheck (prop_PotAssoc:: Positive Int -> Positive Int -> Positive Int -> Bool)
+  quickCheck (prop_RevRev::String -> Bool)
+  quickCheck (prop_F::Fun Char Int -> Char -> Bool)
+  quickCheck (prop_FComp::Fun Char Int -> Fun Int String -> Char -> Bool)
+  quickCheck (prop_foldr1::String -> String -> Bool)
+  quickCheck (prop_foldr2::[String] -> Bool)
+
