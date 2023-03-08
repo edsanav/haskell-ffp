b8465f8e5b0df0ff2f0bd51dabd1201c60806933
Eduardo Santamaria <edsanav@gmail.com>
2023-03-08 20:19:50 +0100

    More examples

--- src/chapters/ch15/OptMonoid.hs
+++ src/chapters/ch15/OptMonoid.hs
@@ -1,4 +1,15 @@
 module OptMonoid where
+  
+import Test.QuickCheck
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity::(Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity::(Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
 
 data Optional a = Nada | Only a deriving (Eq, Show)
 
@@ -10,3 +21,32 @@ instance Semigroup a => Semigroup (Optional a) where
 
 instance Monoid a => Monoid (Optional a) where
   mempty = Nada
+
+newtype First' a = 
+  First' { getFirst' :: Optional a}
+  deriving (Eq, Show)
+  
+instance Semigroup (First' a) where
+  (<>) fi@(First' (Only _)) _ = fi
+  (<>) (First' Nada) fi@(First' (Only _)) = fi
+  (<>) _ _ = First' Nada
+  
+instance Monoid (First' a) where
+  mempty = First' Nada
+  
+instance (Arbitrary a) => Arbitrary (First' a) where
+  arbitrary = do
+    a <- arbitrary
+    elements [First' Nada, First' (Only a)]
+    
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+type FirstMappend = First' String -> First' String -> First' String -> Bool
+type FstId = First' String -> Bool
+
+check::IO()
+check = do 
+  quickCheck (monoidAssoc :: FirstMappend)
+  quickCheck (monoidLeftIdentity :: FstId)
+  quickCheck (monoidRightIdentity :: FstId)
--- src/chapters/ch15/Scratch.hs
+++ src/chapters/ch15/Scratch.hs
@@ -16,10 +16,26 @@ monoidLeftIdentity a = (mempty <> a) == a
 monoidRightIdentity::(Eq m, Monoid m) => m -> Bool
 monoidRightIdentity a = (a <> mempty) == a
 
+data Bull = Fools | Twoo deriving (Eq, Show)
 
+instance Arbitrary Bull where
+  arbitrary = frequency [(1, return Fools), (1, return Twoo)]
+  
+instance Semigroup Bull where
+  (<>) _ _ = Fools
+  
+instance Monoid Bull where
+  mempty = Fools
+  
+type BullMappend = Bull -> Bull -> Bull -> Bool
+
+  
 check ::IO()
 check = do
   quickCheck (monoidAssoc::MA)
   quickCheck (monoidRightIdentity::String->Bool)
   quickCheck (monoidLeftIdentity::String -> Bool)
+  quickCheck (monoidAssoc::BullMappend)
+  quickCheck (monoidRightIdentity::Bull->Bool)
+  quickCheck (monoidLeftIdentity::Bull -> Bool)
   
