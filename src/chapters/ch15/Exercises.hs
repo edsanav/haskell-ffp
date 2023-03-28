cacc086840e144e1d6a9f430b13d31a455ebc05a
Eduardo Santamaria <edsanav@gmail.com>
2023-03-28 20:49:33 +0200

    Monoids, wip

--- src/chapters/ch15/Exercises.hs
+++ src/chapters/ch15/Exercises.hs
@@ -7,8 +7,17 @@ data Trivial = Trivial deriving (Eq, Show)
 semigroupAssoc::(Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
 
+mli::(Eq m, Monoid m) => m -> Bool
+mli a = (mempty <> a) == a
+
+mlr::(Eq m, Monoid m) => m -> Bool
+mlr a = (a <> mempty) == a
+
 instance Semigroup Trivial where
   _ <> _ = Trivial
+
+instance Monoid Trivial where
+  mempty = Trivial
 
 instance Arbitrary Trivial where
   arbitrary = return Trivial
@@ -20,6 +29,9 @@ newtype Identity a = Identity a deriving (Eq, Show)
 instance  Semigroup a => Semigroup (Identity a) where
   (<>) (Identity x) (Identity y) =  Identity $ (<>) x y
 
+instance Monoid a => Monoid (Identity a) where
+  mempty = Identity mempty
+
 instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary =  Identity <$> arbitrary
 
@@ -29,6 +41,9 @@ data Two a b = Two a b deriving (Eq, Show)
 
 instance  (Semigroup a, Semigroup b) => Semigroup (Two a b) where
  (<>) (Two x y) (Two x' y') =  Two (x <> x') ( y<> y')
+ 
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+  mempty = Two mempty mempty 
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
  arbitrary = Two <$> arbitrary  <*> arbitrary
@@ -45,9 +60,13 @@ newtype BoolConj = BoolConj Bool deriving (Eq, Show)
 
 instance Semigroup BoolConj where
   (<>) (BoolConj a) (BoolConj b) = BoolConj (a && b)
+  
+instance Monoid BoolConj where
+  mempty = BoolConj True
 
 instance Arbitrary BoolConj where
   arbitrary = elements [BoolConj True, BoolConj False]
+
 
 newtype BoolDisj = BoolDisj Bool deriving (Eq, Show)
 
@@ -95,8 +114,13 @@ compEq (Comp f) (Comp g) = property $ \x -> f x == g x
 compAssoc ::(Arbitrary a, Eq a, Show a, Semigroup a) => Comp a -> Comp a -> Comp a -> Property
 compAssoc cf cg ch = compEq ((cf <> cg) <> ch) (cf <> (cg <> ch))
 
+data Validation a b = Fail a | Succ b deriving (Eq, Show)
 
-main :: IO()
+instance Semigroup a => Semigroup (Validation a b) where
+  (<>) (Succ b) _ = Succ b
+  (<>) (Fail a) (Succ b) = Succ b
+  (<>) (Fail a1) (Fail a2) = Fail (a1 <> a2)
+
 main = do
   quickCheck (semigroupAssoc :: TrivAssoc)
   quickCheck (semigroupAssoc :: IdenAssoc String)
@@ -108,3 +132,24 @@ main = do
   -- https://stackoverflow.com/a/27074021 
   quickCheck $ \(Fn f) (Fn g) (Fn h) -> (combineAssoc :: CombineAssoc Int Ordering) (Combine f) (Combine g) (Combine h)
   quickCheck $ \(Fn f) (Fn g) (Fn h) -> (compAssoc :: Comp Ordering -> Comp Ordering -> Comp Ordering -> Property) (Comp f) (Comp g) (Comp h)
+
+  -- Monoids
+  quickCheck (mli :: Trivial -> Bool)
+  quickCheck (mlr :: Trivial -> Bool)
+  quickCheck (mli :: Identity Ordering -> Bool)
+  quickCheck (mlr :: Identity Ordering -> Bool)
+  quickCheck (mli :: Two String Ordering -> Bool)
+  quickCheck (mlr :: Two String Ordering -> Bool)
+
+
+
+
+main2 = do
+  let failure ::String -> Validation String Int
+      failure = Fail
+      success :: Int -> Validation String Int
+      success = Succ
+  print $ success 1 <> failure "blah"
+  print $ failure "woot" <> failure "blah"
+  print $ success 1 <> success 2
+  print $ failure "woot" <> success 2
