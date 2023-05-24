5e90abe98e0648c353641994ff024bff6f52a08a
Eduardo Santamaria <edsanav@gmail.com>
2023-05-24 20:30:06 +0200

    Chapter 16 functors

---
+++ src/chapters/ch16/ReplaceExperiment.hs
@@ -0,0 +1,53 @@
+module ReplaceExperiment where
+
+--(.) :: (b -> c) -> (a -> b) -> a -> c
+--
+--fmap :: Functor f => (m -> n) -> f m -> f n
+--fmap :: Functor g => (x -> y) -> g x -> g y
+
+-- ???
+-- ((m->n) -> fm -> fn) -> ((x -> y) -> gx -> gy) -> (x -> y) -> fm -> fn 
+
+replaceWithP :: b -> Char
+replaceWithP = const 'p'
+
+lms :: [Maybe [Char]]
+lms = [Just "Ave", Nothing, Just "woohoo"]
+
+replaceWithP' :: [Maybe [Char]] -> Char
+replaceWithP' = replaceWithP
+
+liftedReplace :: Functor f => f a -> f Char
+liftedReplace = fmap replaceWithP
+
+liftedReplace' :: [Maybe [Char]] -> [Char]
+liftedReplace' = liftedReplace
+
+twiceLifted :: (Functor f1, Functor f) => f (f1 a) -> f (f1 Char)
+twiceLifted = (fmap . fmap) replaceWithP
+
+twiceLifted' :: [Maybe [Char]] -> [Maybe Char]
+twiceLifted' = twiceLifted
+
+thriceLifted ::(Functor f2, Functor f1, Functor f) => f (f1 (f2 a)) -> f (f1 (f2 Char))
+thriceLifted = (fmap . fmap . fmap) replaceWithP
+
+thriceLifted' :: [Maybe [Char]] -> [Maybe [Char]]
+thriceLifted' = thriceLifted
+
+main :: IO ()
+main = do
+  putStr "replaceWithP' lms: "
+  print (replaceWithP' lms)
+  putStr "liftedReplace lms: "
+  print (liftedReplace lms)
+  putStr "liftedReplace' lms: "
+  print (liftedReplace' lms)
+  putStr "twiceLifted lms: "
+  print (twiceLifted lms)
+  putStr "twiceLifted' lms: "
+  print (twiceLifted' lms)
+  putStr "thriceLifted lms: "
+  print (thriceLifted lms)
+  putStr "thriceLifted' lms: "
+  print (thriceLifted' lms)
