ca1abbe6f33233864f72650207b500b905ec37a0
Eduardo Santamaria <edsanav@gmail.com>
2023-05-25 20:51:13 +0200

    Chapter 16, problems with laws

---
+++ src/chapters/ch16/Laws.hs
@@ -0,0 +1,59 @@
+module Laws where
+
+import Test.QuickCheck
+
+
+-- Laws are
+-- fmap id = id
+-- fmap (p . q) = (fmap p) . (fmap q)
+
+functorIdentity::(Eq (f b), Functor f) => f b -> Bool
+functorIdentity f1 = fmap id f1 == id f1
+
+functorCompose::(Eq (f c), Functor f)  => (a ->b ) -> (b -> c) -> f a -> Bool
+functorCompose f g x = (fmap (g . f) x) == (fmap g (fmap f x))
+
+{-
+review this
+https://github.com/yamad/haskellbook-hpfp/blob/master/Ex16.hs
+
+functorCompose'
+  :: (Eq (f c), Functor f)
+  => f a -> Fun a b -> Fun b c -> Bool
+functorCompose' x (Fun _ f) (Fun _ g) =
+  (fmap (g . f) x) == (fmap g . fmap f $ x)
+-}
+
+
+test1 :: [Int] -> Bool
+test1 x = functorIdentity x
+
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = Identity <$> arbitrary
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+test2 :: (Int -> String) -> (String -> Bool) -> Identity Int -> Bool
+test2 = functorCompose
+
+
+{-
+1. newtype Identity a = Identity a
+2. data Pair a = Pair a a
+3. data Two a b = Two a b
+4. data Three a b c = Three a b c
+5. data Three' a b = Three' a b b
+6. data Four a b c d = Four a b c d
+7. data Four' a b = Four' a a a b
+8. Can you implement one for this type? Why? Why not?
+data Trivial = Trivial
+-}
+
+runTest = do
+  quickCheck test1
+  quickCheck (functorIdentity:: Identity Int -> Bool)
+--  quickCheck (functorCompose':: IdentityFC)
+--  quickCheck $ \(Fn f) (Fn g) x -> functorCompose:: 
---
+++ src/chapters/ch16/Scratch.hs
@@ -0,0 +1,13 @@
+module Scratch where
+
+a = (+1) <$> read "[1]"::[Int]
+
+b = (fmap . fmap) (++ "lol") (Just ["Hi,", "Hello"])
+
+c = (*2) . (\x -> x - 2)
+
+d = ((return '1' ++) . show) . (\x -> [x, 1..3])
+
+e = let ioi = readIO "1" :: IO Integer
+        changed = fmap (read  . ("123"++) . show)  ioi
+  in fmap (*3) changed
