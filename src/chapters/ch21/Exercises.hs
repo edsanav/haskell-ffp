11fedb4cfe50904cb40ec652bf9adc5c2b28c7d5
Eduardo Santamaria <edsanav@gmail.com>
2023-09-20 20:38:21 +0200

    More instances

--- src/chapters/ch21/Exercises.hs
+++ src/chapters/ch21/Exercises.hs
@@ -42,9 +42,58 @@ instance Traversable (Constant a) where
   traverse _ (Constant a) = pure (Constant a)
 
 ------------------------------------------------------------------------------------------
+data Optional a = Nada | Yep a deriving (Eq, Ord, Show)
 
+instance (Arbitrary a) => Arbitrary (Optional a) where
+   arbitrary = do
+      a <- arbitrary
+      elements [Nada, Yep a]
+--  arbitrary = arbitrary >>= (\x -> elements [Nada, Yep x])
+
+instance (Eq a) => EqProp (Optional a ) where
+  (=-=) = eq
+
+instance Functor Optional where
+  fmap _ Nada = Nada
+  fmap f (Yep a) = Yep (f a)
+
+instance Foldable Optional where
+  foldMap _ Nada = mempty
+  foldMap f (Yep a) = f a
+
+instance Traversable Optional where
+  traverse _ Nada = pure Nada
+  traverse f (Yep a) = Yep <$> f a
+
+------------------------------------------------------------------------------------------
+data List a = Nil | Cons a (List a) deriving (Eq, Ord, Show)
+
+instance (Arbitrary a) => Arbitrary (List a) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [Nil, Cons a b]
+
+instance (Eq a) => EqProp (List a) where
+  (=-=) = eq
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons a b) = Cons (f a) (fmap f b)
+
+instance Foldable List where
+  foldMap _ Nil = mempty
+  foldMap f (Cons x xs) = f x <> foldMap f xs
+
+instance Traversable List where
+  traverse _ Nil = pure Nil
+  -- Cons <$> f x has type f (List b -> List b) thats why we combine with "traverse f xs" through <*>  
+  traverse f (Cons x xs) = Cons <$> f x <*> traverse f xs
   
 
 
 main = do
   quickBatch $ traversable (undefined:: Identity (Int, Int, [Int]))
+  quickBatch $ traversable (undefined:: Constant Int (Int, Int, [Int]))
+  quickBatch $ traversable (undefined:: Optional (Int, Int, [Int]))
+  quickBatch $ traversable (undefined:: List (Int, Int, [Int]))
