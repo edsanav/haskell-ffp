3de5ce27e05e51585a9684db4c63b1a74ba3393f
Eduardo Santamaria <edsanav@gmail.com>
2023-10-10 20:23:26 +0200

    Exercises

--- src/chapters/ch22/ReaderInstances.hs
+++ src/chapters/ch22/ReaderInstances.hs
@@ -2,29 +2,51 @@
 
 module ReaderInstances where
 
-newtype Reader r a =
-  Reader { runReader :: r -> a }
-
+newtype Reader r a = Reader {runReader :: r -> a}
 
 instance Functor (Reader r) where
-  fmap f (Reader ra) = Reader (f.ra)
+  fmap f (Reader ra) = Reader (f . ra)
 
 instance Applicative (Reader r) where
-
-  pure::a -> Reader r a
+  pure :: a -> Reader r a
   pure a = Reader $ const a -- same as Reader $ \_ -> a
 
-  (<*>)::Reader r (a->b) -> Reader r a -> Reader r b
+  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
   (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r)
 
 instance Monad (Reader r) where
   return = pure
-  (>>=) :: Reader r a-> (a -> Reader r b) -> Reader r b
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
   (Reader ra) >>= aRb = Reader $ \r -> (runReader $ aRb $ ra r) r
-  
-myLiftA2::Applicative f => (a -> b -> c) -> f a -> f b -> f c
+
+myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 myLiftA2 f fa fb = f <$> fa <*> fb
 
 ask :: (r -> a) -> Reader r a
 ask = Reader
 
+newtype HumanName = HumanName String deriving (Eq, Show)
+
+newtype DogName = DogName String deriving (Eq, Show)
+
+newtype Address = Address String deriving (Eq, Show)
+
+data Person = Person {humanName :: HumanName, dogName :: DogName, address :: Address} deriving (Eq, Show)
+
+data Dog = Dog
+  { dogsName :: DogName,
+    dogsAddress :: Address
+  }
+  deriving (Eq, Show)
+
+getDogRM :: Person -> Dog
+getDogRM = do
+  name <- dogName
+  addy <- address
+  return $ Dog name addy
+  
+getDog2:: Person -> Dog
+getDog2 = myLiftA2 Dog dogName address
+
+getDog3::Person -> Dog
+getDog3 = Dog <$> dogName <*> address
---
+++ src/chapters/ch22/ReaderPractice.hs
@@ -0,0 +1,9 @@
+module ReaderPractice where
+import Control.Applicative
+import Data.Maybe
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+lookup :: Eq a => a -> [(a, b)] -> Maybe b
