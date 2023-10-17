8398958e222038a0cd91f33391ff9b14d73a9617
Eduardo Santamaria <edsanav@gmail.com>
2023-10-17 20:36:58 +0200

    Exercises

--- src/chapters/ch22/ReaderPractice.hs
+++ src/chapters/ch22/ReaderPractice.hs
@@ -6,4 +6,38 @@ x = [1, 2, 3]
 y = [4, 5, 6]
 z = [7, 8, 9]
 
-lookup :: Eq a => a -> [(a, b)] -> Maybe b
+
+xs::Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys:: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs:: Maybe Integer
+zs = lookup 4 $ zip x y
+
+z':: Integer -> Maybe Integer
+z' = flip lookup (zip x z)
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+-- TODO probably there's a nicer way
+x3 :: Integer -> Maybe (Integer, Integer)
+x3 n = (,) <$> z' n <*> z' n
+
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+-- Remember functions are applicatives
+bolt:: Integer -> Bool
+bolt = liftA2 (&&) (>3) (<8)
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe _ (Just x') = x'
+fromMaybe x' Nothing = x'
+
