2ae3f97d1e5625353d772bccad9d0bbf5444bb0f
Eduardo Santamaria <edsanav@gmail.com>
2023-10-19 20:22:15 +0200

    Just starting state monad

---
+++ src/chapters/ch23/Example.hs
@@ -0,0 +1,12 @@
+module Example where
+
+import System.Random
+
+a = mkStdGen 0 :: StdGen -- two Int32 values
+
+-- next produces Int (random value) and a StdGen
+b = next(a)
+
+c = snd b
+
+d = random a:: (Char, StdGen)
