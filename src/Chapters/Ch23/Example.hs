41cc89f627948ebe1a4468a73f6d71f9e485f473
Eduardo Santamaria <edsanav@gmail.com>
2023-11-02 13:43:07 +0100

    Setting up vscode HLS

--- src/Chapters/Ch23/Example.hs
+++ src/Chapters/Ch23/Example.hs
@@ -1,11 +1,11 @@
-module Example where
+module Chapters.Ch23.Example where
 
 import System.Random
 
 a = mkStdGen 0 :: StdGen -- two Int32 values
 
 -- next produces Int (random value) and a StdGen
-b = next(a)
+b = next a
 
 c = snd b
 
