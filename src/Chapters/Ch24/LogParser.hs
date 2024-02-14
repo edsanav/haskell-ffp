39a15319d998fe72aa8385480c3552c63429c8a1
Eduardo Santamaria <edsanav@gmail.com>
2024-02-14 19:27:11 +0100

    Nearly got the entry, cleanup ending space

--- src/Chapters/Ch24/LogParser.hs
+++ src/Chapters/Ch24/LogParser.hs
@@ -2,10 +2,13 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module Chapters.Ch24.LogParser where
+import Control.Applicative
 
 import Data.ByteString (ByteString)
 import Text.RawString.QQ
 import qualified Data.Map as M
+
+import Chapters.Ch24.Utils (parseNDigits, rpbs, rp)
 
 import Text.Trifecta
 
@@ -38,18 +41,63 @@ import Text.Trifecta
 --21:15 Read
 --22:00 Sleep
 -- |]
+-- 
+exDate :: ByteString
+exDate = [r|# 2025-02-07 -- dates not nececessarily sequential
+|]
+
+exEntry::ByteString
+exEntry = [r|08:00 Breakfast -- should I try skippin bfast?
+09:00 Bumped head, passed out
+13:36 Wake up, headache
+|]
 
 -- Maybe newtypes if we want to define show?
-type Year = Int
-type Month = Int
-type Day = Int
+type Year = Integer
+type Month = Integer
+type Day = Integer
 
-type Hour = Int
-type Minute = Int
+type Hour = Integer
+type Minute = Integer
 type Activity = String
 
 data Date = Date Year Month Day deriving (Eq, Show, Ord)
 data Time = Time Hour Minute deriving (Eq, Show, Ord)
 
-type Entries = M.Map Time Activity
+type Entries = [(Time, Activity)]
 newtype Log = Log (M.Map Date Entries) deriving (Eq, Show)
+
+skipEOL::Parser()
+skipEOL = skipMany (oneOf "\n")
+
+skipComments:: Parser ()
+skipComments =  skipMany (string "--" *> skipMany (noneOf "\n"))
+
+
+parseTime:: Parser Date
+parseTime = do
+  _ <- string "# "
+  year <- decimal
+  _ <- char '-'
+  month <- decimal
+  _ <- char '-'
+  day <- decimal
+  return $ Date year month day
+  
+parseActivity::Parser Activity
+parseActivity = undefined
+
+parseEntry::Parser (Time, Activity)
+parseEntry = do
+  hours <- decimal
+  _ <- char ':'
+  minutes <- decimal
+  _ <- char ' '
+  activity <- some (noneOf ("\n" <|> "--" )) <* optional (char ' ') <* optional skipComments
+  _ <- skipEOL
+  return (Time hours minutes , activity)
+  
+main::IO()
+main = do 
+  print $ rpbs parseTime exDate
+  print $ rpbs (some parseEntry) exEntry
--- src/Chapters/Ch24/Utils.hs
+++ src/Chapters/Ch24/Utils.hs
@@ -1,8 +1,14 @@
-module Chapters.Ch24.Utils(rp, parseNDigits) where
+module Chapters.Ch24.Utils(rp, parseNDigits, rpbs) where
 import Text.Trifecta
+import Data.ByteString (ByteString)
+
 
 rp :: Parser a -> String -> Result a
 rp f = parseString f mempty
+
+rpbs::Parser a -> ByteString -> Result a
+rpbs f = parseByteString f mempty
+
 
 parseNDigits:: Int ->  Parser Int
 parseNDigits n = read <$> count n digit
