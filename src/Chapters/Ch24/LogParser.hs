4d9d8d1fb8f78fe13eafd25263c866ebc9a3237c
Eduardo Santamaria <edsanav@gmail.com>
2024-02-15 19:44:25 +0100

    Everything except comments

--- src/Chapters/Ch24/LogParser.hs
+++ src/Chapters/Ch24/LogParser.hs
@@ -12,44 +12,64 @@ import Chapters.Ch24.Utils (parseNDigits, rpbs, rp)
 
 import Text.Trifecta
 
---example :: ByteString
---example = [r|
----- wheee a comment
---
--- # 2025-02-05
---08:00 Breakfast
---09:00 Sanitizing moisture collector
---11:00 Exercising in high-grav gym
---12:00 Lunch
---13:00 Programming
---17:00 Commuting home in rover
---17:30 R&R
---19:00 Dinner
---21:00 Shower
---21:15 Read
---22:00 Sleep
---
--- # 2025-02-07 -- dates not nececessarily sequential
---08:00 Breakfast -- should I try skippin bfast?
---09:00 Bumped head, passed out
---13:36 Wake up, headache
---13:37 Go to medbay
---13:40 Patch self up
---13:45 Commute home for rest
---14:15 Read
---21:00 Dinner
---21:15 Read
---22:00 Sleep
--- |]
--- 
+example :: ByteString
+example = [r|-- wheee a comment
+
+\# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
+
+\# 2025-02-07 -- dates not nececessarily sequential
+08:00 Breakfast -- should I try skippin bfast?
+09:00 Bumped head, passed out
+13:36 Wake up, headache
+13:37 Go to medbay
+13:40 Patch self up
+13:45 Commute home for rest
+14:15 Read
+21:00 Dinner
+21:15 Read
+22:00 Sleep
+|]
+
+
 exDate :: ByteString
-exDate = [r|# 2025-02-07 -- dates not nececessarily sequential
+exDate = [r|\# 2025-02-07 -- dates not nececessarily sequential
 |]
 
 exEntry::ByteString
 exEntry = [r|08:00 Breakfast -- should I try skippin bfast?
 09:00 Bumped head, passed out
 13:36 Wake up, headache
+13:37 Go to medbay
+13:40 Patch self up
+13:45 Commute home for rest
+14:15 Read
+21:00 Dinner
+21:15 Read
+22:00 Sleep
+|]
+exEntry2::ByteString
+exEntry2 = [r|08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
 |]
 
 -- Maybe newtypes if we want to define show?
@@ -76,16 +96,23 @@ skipComments =  skipMany (string "--" *> skipMany (noneOf "\n"))
 
 parseTime:: Parser Date
 parseTime = do
-  _ <- string "# "
+  _ <- string "\\# "
   year <- decimal
   _ <- char '-'
   month <- decimal
   _ <- char '-'
   day <- decimal
+  _ <- skipMany (char ' ')
   return $ Date year month day
   
+singleDash:: Parser Char
+singleDash = char '-' <* notFollowedBy (char '-')
+
+anyWord::Parser String
+anyWord = some $ try (alphaNum <|> char ' ' <|> singleDash)
+
 parseActivity::Parser Activity
-parseActivity = undefined
+parseActivity = some (noneOf ("\n")) <* optional (char ' ') <* optional skipComments
 
 parseEntry::Parser (Time, Activity)
 parseEntry = do
@@ -93,11 +120,27 @@ parseEntry = do
   _ <- char ':'
   minutes <- decimal
   _ <- char ' '
-  activity <- some (noneOf ("\n" <|> "--" )) <* optional (char ' ') <* optional skipComments
+  activity <- parseActivity
   _ <- skipEOL
   return (Time hours minutes , activity)
+
+parseSection::Parser (Date, Entries)
+parseSection = do
+  _ <- skipComments
+  _ <- skipEOL
+  date <- parseTime <*skipComments <* skipEOL
+  entries <- some parseEntry
+  return (date, entries)
+
+parseLog::Parser Log
+parseLog = do
+  sections <- some parseSection
+  let mapofSections = M.fromList sections
+  return $ Log mapofSections
   
 main::IO()
 main = do 
   print $ rpbs parseTime exDate
   print $ rpbs (some parseEntry) exEntry
+  print $ rpbs (some parseEntry) exEntry2
+  print $ rpbs parseLog example
