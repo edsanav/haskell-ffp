3c123c206d6a5e7ad966227e2a67f2a3df1c71a7
Eduardo Santamaria <edsanav@gmail.com>
2024-03-25 20:17:27 +0100

    Brainfu

--- src/Chapters/Ch26/WrapItUp.hs
+++ src/Chapters/Ch26/WrapItUp.hs
@@ -5,5 +5,22 @@ import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Reader
 
 embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int
-embedded = undefined
---embedded = ( MaybeT (ExceptT (ReaderT (const (Right (Just 1))))))
+embedded = MaybeT (ExceptT (ReaderT (const (return $ Right (Just 1)))))
+
+-- It's a matter of adding layers.
+-- First what we want to wrap
+-- IO (Either (String (Maybe (Int)))
+inner::() -> IO(Either String (Maybe Int))
+inner = const $ return (Right (Just 1))
+
+-- then we add first layer of transformers, reader
+rd :: ReaderT () IO(Either String (Maybe Int))
+rd = ReaderT inner
+
+-- then exceptT, note types are "moving" from the inner wrapped to the transformers
+exc :: ExceptT String (ReaderT () IO) (Maybe Int)
+exc = ExceptT rd
+
+-- then the maybe 
+mb:: MaybeT (ExceptT String (ReaderT () IO)) Int
+mb = MaybeT exc
