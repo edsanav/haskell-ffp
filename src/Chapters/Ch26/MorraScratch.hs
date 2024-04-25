f27f8d1794baef412578298519fb22d17493c65e
Eduardo Santamaria <edsanav@gmail.com>
2024-04-25 20:30:53 +0200

    Just a play

--- src/Chapters/Ch26/MorraScratch.hs
+++ src/Chapters/Ch26/MorraScratch.hs
@@ -6,6 +6,22 @@ import Control.Monad (unless)
   
 type IOState s = StateT s IO 
 
+data Choice = Odds | Evens deriving (Show, Eq)
+
+data Player = Player String Choice deriving (Show, Eq)
+
+data Play = Play Player Int deriving (Show, Eq)
+
+--data Counter = 
+
+play:: Play -> Play -> Player
+play (Play p1@(Player _ c1) x) (Play p2@(Player _ _) y) = winner
+  where evenResult = even (x + y) 
+        winner = if (evenResult && c1 == Evens) || (not evenResult && c1 == Odds) 
+                  then p1 
+                  else p2
+
+
 loop :: IOState () ()
 loop = do 
   _ <- lift $ putStrLn "Choose a number between 0 and 5"
