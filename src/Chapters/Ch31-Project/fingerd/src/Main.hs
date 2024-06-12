c29a5183e25bee94ea4555af5fffeff5e9119823
Eduardo Santamaria <edsanav@gmail.com>
2024-06-12 19:46:05 +0200

    Template finished

--- src/Chapters/Ch31-Project/fingerd/src/Main.hs
+++ src/Chapters/Ch31-Project/fingerd/src/Main.hs
@@ -15,10 +15,12 @@ import Database.SQLite.Simple hiding (close)
 import qualified Database.SQLite.Simple as SQLite
 import Database.SQLite.Simple.Types
 import Network.Socket hiding (close, recv)
+import qualified Network.Socket as S
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Network.Socket.ByteString (recv, sendAll)
 import Text.RawString.QQ
+import Data.ByteString.Char8 (putStrLn)
 
 data User = User {
     userId :: Integer
@@ -86,5 +88,62 @@ createDatabase = do
   where meRow::UserRow
         meRow = (Null, "edsanav", "/bin/zsh", "/home/eduardo", "Eduardo Santmaria", "941342312")
 
+
+formatUser :: User -> ByteString
+formatUser (User _ username shell homeDir realName _) = BS.concat
+ [ "Login: ", e username, "\t\t\t\t",
+   "Name: ", e realName, "\n",
+   "Directory: ", e homeDir, "\t\t\t\t",
+   "Shell: ", e shell,  "\n"
+ ]
+ where e = encodeUtf8
+
+
+returnUsers::Connection -> Socket -> IO ()
+returnUsers dbConn soc = do
+  rows <- query_ dbConn allUsers
+
+  let usernames = map username rows
+      newLineSeparated = T.concat $ intersperse "\n" usernames
+  sendAll soc (encodeUtf8 newLineSeparated)
+
+returnUser:: Connection -> Socket -> Text -> IO ()
+returnUser dbConn soc username = do
+  maybeUser <- getUser dbConn (T.strip username)
+  case maybeUser of
+    Nothing -> do
+      Prelude.putStrLn ("Couldn't find matching user for username:" ++ show (T.strip username))
+      return ()
+    Just user -> sendAll soc (formatUser user)
+
+handleQuery :: Connection -> Socket -> IO ()
+handleQuery dbConn soc = do
+  msg <- recv soc 1024
+  case msg of
+    "\r\n" -> returnUsers dbConn soc
+    name -> returnUser dbConn soc (decodeUtf8 name)
+
+handleQueries :: Connection -> Socket -> IO ()
+handleQueries dbConn sock = forever $ do
+  (soc, _) <- accept sock
+  Prelude.putStrLn "Got connection, handle query"
+  handleQuery dbConn soc
+  S.close soc
+
+
 main :: IO ()
-main = createDatabase
+main = withSocketsDo $ do
+  addrinfos <- getAddrInfo (Just (defaultHints {addrFlags =[AI_PASSIVE]})) Nothing (Just "79")
+
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+  S.bind sock $ addrAddress serveraddr
+  listen sock 1
+
+  conn <- open "finger.db"
+  handleQueries conn sock
+
+  SQLite.close conn
+  S.close sock
+
+-- Further reading: https://www.fpcomplete.com/blog/async-exception-handling-haskell/
